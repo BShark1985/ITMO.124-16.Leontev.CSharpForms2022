3dc5750900c26a089a7e50a55b4403fcc307ffcb
BShark1985 <leonand1985@gmail.com>
2022-07-06 21:04:24 +0300

    Добавьте файлы проекта.

---
+++ LAB_01-03/nForm.cs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LAB_01_03
+{
+    public partial class nForm : LAB_01_03.Form1
+    {
+        public nForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
